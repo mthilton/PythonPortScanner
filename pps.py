c476f18f2ab6258608931b518fb4967603f9982d
mthilton <mthilton@ucsc.edu>
2025-04-23 11:27:49 -0700

    More advanced port scanning being able to give what service is there (kinda) and translate FQDN to IPv4 addr.

--- pps.py
+++ pps.py
@@ -24,22 +24,36 @@ def check_ip(ip: str) -> bool:
         return False
 
 
-async def test_port(host: str, port: int) -> tuple[int, str]:
+async def test_port(host: str, port: int) -> tuple[str, str, str]:
     '''Creates a worker socket for async'''
     loop = asyncio.get_event_loop()
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setblocking(False)
+    port_str = str(port)
+    port_state = None
+    port_service = None
+
+    # Is the Port open?
     try:
         await loop.sock_connect(s, (host, port))
-        res = (port, "Open")
+        port_state = "Open"
     except ConnectionRefusedError:
-        res = (port, "Refused")
+        port_state = "Refused"
     except TimeoutError:
-        res = (port, "Timeout")
-    finally:
-        # closes te object
-        s.close()
-    return res
+        port_state = "Timeout"
+
+    # If open then what is at said port
+    if port_state == "Open":
+        try:
+            port_service = socket.getservbyport(port, "tcp")
+            port_str += "/tcp"
+
+        except OSError:
+            port_service = "Unknown"
+
+    s.close()
+
+    return port_str, port_state, port_service
 
 # def handle_args() -> tuple[str, str | None, int | None]:
 #     '''This function is used to handle passed in or incorrect arguments'''
@@ -86,46 +100,59 @@ async def test_port(host: str, port: int) -> tuple[int, str]:
 async def main() -> None:
     '''This is the main function'''
 
-    host = sys.argv[1]
-    if not check_ip(host):
-        raise OSError(f"{host} is not a valid IPv4 Addr")
+    loc = sys.argv[1]
 
-    mode = "c"
+    if check_ip(loc):
+        host = loc
+    else:
+        host = socket.gethostbyname(loc)
+
+    mode = "v"
     # returns 0 if connection succeeds else raises error
     # address and port in the tuple format
     socket.setdefaulttimeout(1)
     iters: int = (MAX_PORT_NUMBER // MAX_NUM_OPEN_SOCK) + 1
-    opened = []
-    timed_out = []
-    refused = []
+    opened = 0
+    timed_out = 0
+    refused = 0
+    processed_ports = {}
+    print(f"Using address: {host}")
+    print("="*50)
     for i in range(iters):
         low_bound = i * MAX_NUM_OPEN_SOCK
         high_bound = min(low_bound + MAX_NUM_OPEN_SOCK, MAX_PORT_NUMBER)
         print(f"Checking Ports {low_bound}:{high_bound}", end="\r")
         tasks = [test_port(host, p) for p in range(low_bound, high_bound)]
         results = await asyncio.gather(*tasks)
+
         for r in results:
-            if r[1] == "Open":
-                opened.append(r[0])
-            if r[1] == "Refused":
-                refused.append(r[0])
-            if r[1] == "Timeout":
-                timed_out.append(r[0])
+            processed_ports[r[0]] = (r[1], r[2])
+            match r[1]:
+                case "Open":
+                    opened += 1
+                case "Refused":
+                    refused += 1
+                case "Timeout":
+                    timed_out += 1
+
         if high_bound == MAX_PORT_NUMBER:
             print(f"Checking Ports {low_bound}:{high_bound}")
 
     print("="*50)
     print("Done!")
     print("="*50)
+    searchspace = opened + timed_out + refused
     if mode == "c":
-        searchspace = len(opened) + len(timed_out) + len(refused)
-        print(f"Open Ports: {len(opened)}/{searchspace}")
-        print(f"Timsed Out Ports: {len(timed_out)}/{searchspace}")
-        print(f"Closed Ports: {len(refused)}/{searchspace}")
+        print(f"Open Ports: {opened}/{searchspace}")
+        print(f"Timsed Out Ports: {timed_out}/{searchspace}")
+        print(f"Closed Ports: {refused}/{searchspace}")
     else:
-        print(f"Open Ports: {opened}")
-        print(f"Timed Out Ports: {timed_out}")
-        print(f"Closed Ports: {refused}")
+        print(f"Closed Ports: {refused}/{searchspace}")
+        print(f"Open Ports:\n{'Port': <8} {'State': <8} Service")
+        for port, port_info in processed_ports.items():
+            if port_info[0] == "Refused":
+                continue
+            print(f"{port: <8} {port_info[0]: <8} {port_info[1]}")
 
 
 if __name__ == "__main__":
