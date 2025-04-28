01353c11dc8278fa145700f21bc53f329de38b92
mthilton <mthilton@ucsc.edu>
2025-04-28 12:13:35 -0700

    added Command line args using argparse

--- pps.py
+++ pps.py
@@ -4,11 +4,10 @@
    Usage: pps [c|v] <address> [-p 0-65535]
 '''
 
-import sys
 import socket
 import asyncio
+import argparse
 
-MAX_PORT_NUMBER = 65536
 MAX_NUM_OPEN_SOCK = 100
 
 
@@ -21,13 +20,13 @@ def check_ip(ip: str) -> bool:
         return False
 
 
-async def test_port(host: str, port: int) -> tuple[str, str, str]:
+async def test_port(host: str, port: int) -> tuple[str, str | None, str | None]:
     '''Creates a worker socket for async'''
     loop = asyncio.get_event_loop()
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setblocking(False)
     socket.setdefaulttimeout(1)
-    port_str = str(port) + "\\tcp"
+    port_str = str(port) + "/tcp"
     port_state = None
     port_service = None
 
@@ -48,28 +47,21 @@ async def test_port(host: str, port: int) -> tuple[str, str, str]:
     return port_str, port_state, port_service
 
 
-async def main() -> None:
+async def main(host: str, start_port: int, end_port: int, verbose: bool) -> None:
     '''This is the main function'''
 
-    loc = sys.argv[1]
-    if check_ip(loc):
-        host = loc
-    else:
-        host = socket.gethostbyname(loc)
+    if not check_ip(host):
+        host = socket.gethostbyname(host)
 
-    mode = "v"
-    # returns 0 if connection succeeds else raises error
-    # address and port in the tuple format
-    iters: int = (MAX_PORT_NUMBER // MAX_NUM_OPEN_SOCK) + 1
-    opened = 0
+    opened = []
     timed_out = 0
     refused = 0
     processed_ports = {}
     print(f"Using address: {host}")
     print("="*50)
-    for i in range(iters):
-        low_bound = i * MAX_NUM_OPEN_SOCK
-        high_bound = min(low_bound + MAX_NUM_OPEN_SOCK, MAX_PORT_NUMBER)
+    for i in range(start_port, end_port, MAX_NUM_OPEN_SOCK):
+        low_bound = i
+        high_bound = min(low_bound + MAX_NUM_OPEN_SOCK, end_port)
         print(f"Checking Ports {low_bound}:{high_bound}", end="\r")
         tasks = [test_port(host, p) for p in range(low_bound, high_bound)]
         results = await asyncio.gather(*tasks)
@@ -78,23 +70,24 @@ async def main() -> None:
             processed_ports[r[0]] = (r[1], r[2])
             match r[1]:
                 case "Open":
-                    opened += 1
+                    opened.append(r[0].replace("/tcp", ""))
                 case "Refused":
                     refused += 1
                 case "Timeout":
                     timed_out += 1
 
-        if high_bound == MAX_PORT_NUMBER:
+        if high_bound == end_port:
             print(f"Checking Ports {low_bound}:{high_bound}")
 
     print("="*50)
     print("Done!")
     print("="*50)
-    searchspace = opened + timed_out + refused
-    if mode == "c":
-        print(f"Open Ports: {opened}/{searchspace}")
-        print(f"Timsed Out Ports: {timed_out}/{searchspace}")
+    searchspace = len(opened) + timed_out + refused
+
+    if not verbose:
         print(f"Closed Ports: {refused}/{searchspace}")
+        print(f"Timsed Out Ports: {timed_out}/{searchspace}")
+        print(f"Open Ports: {len(opened)}/{searchspace}\n{opened}")
     else:
         print(f"Closed Ports: {refused}/{searchspace}")
         print(f"Open Ports:\n{'Port': <8} {'State': <8} Service")
@@ -106,4 +99,34 @@ async def main() -> None:
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    parser = argparse.ArgumentParser(
+        description="Scans open ports on given host")
+    parser.add_argument(
+        "host",
+        help="Select the host that you would like to scan. Can be IPv4 addr or an FQDN.",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        help="Shows detailed port information, rather than just lists open ports.",
+        action="store_true"
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        help="Specifiy what port(s) to search thourgh. Default = 1-65536",
+        dest="port_range",
+        default="1-65536"
+    )
+    args = parser.parse_args()
+
+    if args.port_range.index("-") > -1:
+        start, end = args.port_range.split("-")
+        sp = int(start)
+        ep = int(end)
+    elif args.port_range.isnumeric():
+        sp = ep = int(args.port_range)
+    else:
+        raise OSError("Invaid port Selection")
+
+    asyncio.run(main(args.host, sp, ep, args.verbose))
