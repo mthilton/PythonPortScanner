80046ccc67b127b5ad04a54c6b3e4cc03ecf777c
mthilton <mthilton@ucsc.edu>
2025-04-28 11:10:07 -0700

    Cleaned up the codebase, finalised how port scanning will work.

--- pps.py
+++ pps.py
@@ -5,12 +5,9 @@
 '''
 
 import sys
-# import re
 import socket
 import asyncio
 
-
-# USAGE = "Usage: pps [c|v] <address> [p 0-65535]"
 MAX_PORT_NUMBER = 65536
 MAX_NUM_OPEN_SOCK = 100
 
@@ -29,7 +26,8 @@ async def test_port(host: str, port: int) -> tuple[str, str, str]:
     loop = asyncio.get_event_loop()
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setblocking(False)
-    port_str = str(port)
+    socket.setdefaulttimeout(1)
+    port_str = str(port) + "\\tcp"
     port_state = None
     port_service = None
 
@@ -37,71 +35,23 @@ async def test_port(host: str, port: int) -> tuple[str, str, str]:
     try:
         await loop.sock_connect(s, (host, port))
         port_state = "Open"
+        port_service = socket.getservbyport(port)
     except ConnectionRefusedError:
         port_state = "Refused"
     except TimeoutError:
         port_state = "Timeout"
-
-    # If open then what is at said port
-    if port_state == "Open":
-        try:
-            port_service = socket.getservbyport(port, "tcp")
-            port_str += "/tcp"
-
-        except OSError:
-            port_service = "Unknown"
-
-    s.close()
+    except OSError:
+        port_service = "Unknown"
+    finally:
+        s.close()
 
     return port_str, port_state, port_service
-
-# def handle_args() -> tuple[str, str | None, int | None]:
-#     '''This function is used to handle passed in or incorrect arguments'''
-#     mode = "c"
-#     ip = None
-#     port = None
-#     for arg in sys.argv:
-#         arg.replace("-", "")
-
-#         # arg 0 is always the name of the program
-#         if arg.find("pps") > -1:
-#             continue
-
-#         # Set Mode
-#         if re.match(r"[c|v]{1}", arg.lower()):
-#             mode = arg
-#             continue
-
-#         # Set IP
-#         ip = check_ip(arg)
-#         if not ip:
-#             raise OSError(f"{arg} is not a valid IP")
-
-#         # Set port number if supplied
-#         port_args = arg.split()
-#         opt = port_args[0].lower()
-#         port_num = 0
-#         if opt == "p":
-#             try:
-#                 port_num = int(port_args[2])
-#             except ValueError as e:
-#                 raise ValueError(f"Invaled Argument {arg}\n{USAGE}") from e
-
-#         if 0 <= port_num < MAX_PORT_NUMBER:
-#             port = port_num
-#             continue
-
-#         # If we get here we did not get a proper argument
-#         raise ValueError(f"Invaled Argument {arg}\n{USAGE}")
-
-#     return mode, ip, port
 
 
 async def main() -> None:
     '''This is the main function'''
 
     loc = sys.argv[1]
-
     if check_ip(loc):
         host = loc
     else:
@@ -110,7 +60,6 @@ async def main() -> None:
     mode = "v"
     # returns 0 if connection succeeds else raises error
     # address and port in the tuple format
-    socket.setdefaulttimeout(1)
     iters: int = (MAX_PORT_NUMBER // MAX_NUM_OPEN_SOCK) + 1
     opened = 0
     timed_out = 0
@@ -153,6 +102,7 @@ async def main() -> None:
             if port_info[0] == "Refused":
                 continue
             print(f"{port: <8} {port_info[0]: <8} {port_info[1]}")
+    print("="*50)
 
 
 if __name__ == "__main__":
